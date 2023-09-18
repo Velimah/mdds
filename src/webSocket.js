d93fbd137af92fcfa3a03f457c1a90af231eb781
Veli-Matti Heino <vekkis@gmail.com.com>
2023-09-18 22:32:15 +0300

    11

--- src/webSocket.js
+++ src/webSocket.js
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://mdds-backend.azurewebsites.net/");
+const socket = io("https://mdds-backend.azurewebsites.net", {
+  path: "/my-custom-path/"
+});
 
 export default socket;
