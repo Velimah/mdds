94a8c20607eda99c600866eff5046763bb2780a0
Veli-Matti Heino <vekkis@gmail.com.com>
2023-09-19 06:44:46 +0300

    azure-testi

--- src/webSocket.js
+++ src/webSocket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://mdds-backend.azurewebsites.net", {
-});
+/*const socket = io("http://localhost:3005");*/
+
+ const socket = io("https://mdds-backend.azurewebsites.net");  
 
 export default socket;
