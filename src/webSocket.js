03b0de668586af1b4cf171243a9259ea905922e2
Veli-Matti Heino <vekkis@gmail.com.com>
2023-09-19 21:42:01 +0300

    eitoimi

--- src/webSocket.js
+++ src/webSocket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
 /* const socket = io("https://mediapalvelin.northeurope.cloudapp.azure.com"); */  
-const socket = io("wss://mediapalvelin.northeurope.cloudapp.azure.com");
+const socket = io("http://mediapalvelin.northeurope.cloudapp.azure.com");
 export default socket;
