fd7f21d4071d3a95d048f27756cfdec3dbf3ad72
Veli-Matti Heino <vekkis@gmail.com.com>
2023-09-19 19:59:56 +0300

    asd

--- src/webSocket.js
+++ src/webSocket.js
@@ -1,7 +1,5 @@
 import { io } from "socket.io-client";
 
-/*const socket = io("http://localhost:3005");*/
-
- const socket = io("https://mediapalvelin.northeurope.cloudapp.azure.com");  
-
+/* const socket = io("https://mediapalvelin.northeurope.cloudapp.azure.com"); */  
+const socket = io("https://mediapalvelin.northeurope.cloudapp.azure.com", { path: "/mdds-backend/socket.io" });
 export default socket;
